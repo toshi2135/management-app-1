9b8abbd1cf237b1f3a5268a3e0a2716eae95a0f3
Tuan Nguyen <tuan.nguyen@global-fashion-group.com>
2023-03-27 00:21:58 +0700

    Initial commit

---
+++ management-app/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace management_app
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
