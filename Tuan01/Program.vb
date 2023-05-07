4ac6ac0fb4fd8624cfef5369d69e1f55e2e55c5d
Tuan Nguyen <anhtuan.nguyenduc@gmail.com>
2023-05-07 23:36:56 +0700

    Cong 2 so

--- Tuan01/Program.vb
+++ Tuan01/Program.vb
@@ -2,6 +2,14 @@ Imports System
 
 Module Program
     Sub Main(args As String())
-        Console.WriteLine("Hello World!")
+        Dim a As Double
+        Dim b As Double
+        Console.WriteLine("Chuong trinh cong 2 so")
+        Console.Write("Nhap a: ")
+        a = Double.Parse(Console.ReadLine())
+        Console.Write("Nhap b: ")
+        b = Double.Parse(Console.ReadLine())
+        Console.WriteLine("Tong 2 so la: " & (a + b))
+        Console.ReadLine()
     End Sub
 End Module
