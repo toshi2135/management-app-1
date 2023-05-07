a7d11f719cd706fc5659b7294786ebf74d8aaa60
Tuan Nguyen <anhtuan.nguyenduc@gmail.com>
2023-05-07 23:46:57 +0700

    If statement

--- Tuan01/Program.vb
+++ Tuan01/Program.vb
@@ -2,16 +2,36 @@ Imports System
 
 Module Program
     Sub Main(args As String())
+        'Dim a As Double
+        'Dim b As Double
+        'Dim kq As Double
+        'Console.WriteLine("Chuong trinh cong 2 so")
+        'Console.Write("Nhap a: ")
+        'a = Double.Parse(Console.ReadLine())
+        'Console.Write("Nhap b: ")
+        'b = Double.Parse(Console.ReadLine())
+        'kq = a + b
+        'Dim str As String = String.Format("Ket qua chuong trinh la {0} + {1} = {2}", a, b, kq)
+        'Console.WriteLine(str)
+
         Dim a As Double
         Dim b As Double
-        Dim kq As Double
-        Console.WriteLine("Chuong trinh cong 2 so")
+        Dim kq As String
+        Console.WriteLine("Chuong trinh gia phuong trinh ax + b = 0")
         Console.Write("Nhap a: ")
         a = Double.Parse(Console.ReadLine())
         Console.Write("Nhap b: ")
         b = Double.Parse(Console.ReadLine())
-        kq = a + b
-        Dim str As String = String.Format("Ket qua chuong trinh la {0} + {1} = {2}", a, b, kq)
-        Console.WriteLine(str)
+
+        If a = 0 Then
+            If b = 0 Then
+                kq = String.Format("Phuong trinh co vo so nghiem")
+            Else
+                kq = String.Format("Phuong trinh vo nghiem")
+            End If
+        Else
+            kq = String.Format("Phuong trinh co 1 nghiem x = {0}", -b / a)
+        End If
+        Console.WriteLine(kq)
     End Sub
 End Module
