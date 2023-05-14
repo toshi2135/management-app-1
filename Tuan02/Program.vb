17089c73e76703639e273c95ce69c2cd259c2cc8
Tuan Nguyen <anhtuan.nguyenduc@gmail.com>
2023-05-14 18:54:12 +0700

    Update GiaiPTB1

--- Tuan02/Program.vb
+++ Tuan02/Program.vb
@@ -2,7 +2,21 @@ Imports System
 
 Module Program
     Sub Main(args As String())
-        Console.WriteLine("Hello World!")
+        Console.WriteLine("Chuong trinh giai phuong trinh bac 1")
+        Console.Write("Nhap a: ")
+        Dim a As Double = Double.Parse(Console.ReadLine())
+        Console.Write("Nhap b: ")
+        Dim b As Double = Double.Parse(Console.ReadLine())
+        Dim nghiem As Double
+        Dim SoNghiem As Integer = GiaiPTB1(a, b, nghiem)
+        If SoNghiem = -1 Then
+            Console.WriteLine("Phuong trinh co vo so nghiem")
+        ElseIf SoNghiem = 0 Then
+            Console.WriteLine("Phuong trinh vo nghiem")
+        Else
+            Console.WriteLine("Phuong trinh co 1 nghiem x = {0}", nghiem)
+        End If
+
     End Sub
 
     Function GiaiPTB1(ByVal a As Double, ByVal b As Double, ByRef nghiem As Double) As Integer
